ccf7cfc021bf6d02048eb4505caf8e64a5255166
Rover <rover@codingsans.com>
2016-06-04 23:55:57 +0200

    Response finalizer and register basics

--- auth/register.go
+++ auth/register.go
@@ -1,13 +1,46 @@
 package auth
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
+	"go-auth/utilities/response"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+type requestProperties struct {
+	RealName string `json:"realName"`
+	UserName string `json:"userName"`
+	Password string `json:"password"`
+}
+
+func validateRequest(request requestProperties) error {
+	if request.UserName == "" || request.Password == "" {
+		return errors.New("Request body did not contain userName or password.")
+	}
+	return nil
+}
+
 // RegisterHandler is the handler function of the register endpoint
 func RegisterHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Printf("Register recieved\n")
+	var userInformation requestProperties
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		res.FinalizeError(w, err, http.StatusInternalServerError)
+		return
+	}
+	//var user dbModels.User
+	err = json.Unmarshal(body, &userInformation)
+	if err != nil {
+		res.FinalizeError(w, err, http.StatusInternalServerError)
+		return
+	}
+	err = validateRequest(userInformation)
+	if err != nil {
+		res.FinalizeError(w, err, http.StatusBadRequest)
+		return
+	}
+	res.Finalize(w, userInformation)
 }
